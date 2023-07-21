29bee341d31f80db44d9392e940c6f03f5fb00b7
Orina Duncan <orinaduncansoftware@gmail.com>
2023-07-20 19:58:18 -0700

    0-isupper.c  1-isdigit.c  2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - will be multiplying two integers
+* @a: first integer
+* @b: second integer
+* Return: The result of the multiplication
+*/
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
