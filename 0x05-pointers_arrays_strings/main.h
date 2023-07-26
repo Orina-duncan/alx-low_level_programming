4520c3d357779a4e4cbcf334d34f92e4a4996de8
Orina Duncan <orinaduncansoftware@gmail.com>
2023-07-26 11:13:09 -0700

    main.h edit

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,7 @@
-main.h
-
 #ifndef MAIN.H
 #define MAIN.H
 
 #include <stdio.h>
-#include <unistd.h>
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -18,4 +15,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
-#endif
+#endif/*MAIN.H*/
